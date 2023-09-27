8adc9da7e34e4d582ba00141d45ab17f731c92c8
“Elena <kisek76@mail.ru>
2023-09-27 09:59:27 -0400

    init index.js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const titleModal = document.querySelector('.modal__title');
+const formModal = document.querySelector('.modal__form');
+const labelCheckboxModal = document.querySelectorAll('.modal__text');
+const inputCheckboxModal =
+document.querySelectorAll('.modal__input');
